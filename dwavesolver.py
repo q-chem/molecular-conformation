28d23d9d1169136ab7b9b799da0c7191644176e6
jaykaron <jaykaron@gmail.com>
2020-11-19 20:24:52 -0500

    Add better options handling

--- dwavesolver.py
+++ dwavesolver.py
@@ -7,7 +7,7 @@ from problem import MolecularConformation
 
 class DwaveSolver(MolecularConformation):
     def set_hyper_parameters(self):
-        self.A = 1                      # scalar for constraint matrix
+        self.A = 1000000                      # scalar for constraint matrix
         self.B = 12000                  # scalar for objective matrix
 
     def constraint_Q(self, scale=False):
@@ -60,7 +60,25 @@ class DwaveSolver(MolecularConformation):
         """
         return self.A * self.constraint_Q(scale=True) + self.B * self.objective_Q(scale=True)
 
-    def solve(self, solver='tabu', top_samples=1, visualize=False):
+    # def solve(self, solver='tabu', top_samples=1, visualize=False):
+
+    def solve(self, options):
+        """
+        Options:
+            'solver' - see solver input to Qbsolve.sample, default 'tabu'
+            'top_samples' - int, how many samples to print
+            'visualize' - boolean,
+            'verbosity' - int, default 0 (low)
+        """
+        DEFAULT_OPTIONS = {
+            'solver': 'tabu',
+            'top_samples': 1,
+            'visualize': False,
+            'verbosity': 0
+        }
+        complete_options = DEFAULT_OPTIONS.copy()
+        complete_options.update(options)
+
         # get hamiltonian
         q = self.make_Q()
 
@@ -68,13 +86,19 @@ class DwaveSolver(MolecularConformation):
         Q = dimod.BinaryQuadraticModel.from_numpy_matrix(q)
 
         # solve using QBSolv
-        response = QBSolv().sample(Q, verbosity=0, solver=solver)
+        response = QBSolv().sample(
+            Q,
+            verbosity=complete_options['verbosity'],
+            solver=complete_options['solver']
+        )
 
-        for sample_i, sample in enumerate(response.samples()[0:top_samples]):
+        for sample_i, sample in enumerate(response.samples()[0:complete_options['top_samples']]):
             X = self.sample_to_x_ij_matrix(sample)
             print(f'------- sample {sample_i} -------')
             print('solution is valid:', self.is_solution_valid(X))
             print('energy:', response.record.energy[sample_i])
-            if visualize:
+            print('total U:', self.objective_value(X.flat))
+
+            if complete_options['visualize']:
                 positions = self.sample_to_positions(sample)
                 self.plot_3d(positions)
